db0cc424300ab25fb58689f35fdb09099732fc31
baneon <baneonstore@gmail.com>
2020-11-11 17:14:36 -0400

    Logo
    Beautiful Logo para que sea llamativo.

---
+++ ActualizarHoraWindows/README.md
@@ -0,0 +1,20 @@
+<span style="display:block;text-align:center">![Actualizador de Hora Windows Logo](https://raw.githubusercontent.com/baneonstore/PythonKungFu/main/ActualizarHoraWindows/src/texture/LogoLarge.png)</span>
+
+# Actualizador de Hora Windows
+_Utiliza internet y pide a servidores que sincronize tu hora en Windows._
+
+## Descripción
+. Se libre de manipular y aprender del código o participa y se un colaborador. El Actualizador de Hora Windows es el primero de una serie de herramientas con el que plantear problemas y ejercicios del grupo [Python Kung Fu](t.me/python_kungfu)
+
+### Requisitos
+* pywin32
+
+### Autores
+* [baneon](https://github.com/baneonstore) - *Initial work*
+
+También puede ver la lista de todos los [colaboradores](https://github.com/baneonstore/Digna/contributors) que han participado en este proyecto.
+
+## Lisencia
+Este proyecto esta bajo la lisencia MIT - Ver el archivo [LICENSE.md](LICENSE.md) para más detalles
+
+_This project is under the MIT License - see the file [LICENSE.md](LICENSE.md) for more details_
--- ActualizarHoraWindows/peticion.py
+++ ActualizarHoraWindows/peticion.py
@@ -1,13 +1,13 @@
 # Copyright © 2020 baneon - MIT License
 # See `LICENSE` included in the source distribution for details.
 
-"""Módulo \"peticiones\"
+"""Módulo \"peticion\"
 -------------------
 
 \"main\" utiliza este módulo para hacer peticiones a una
 lista de servidores.
 
-La lista puede ser creada con el módulo de \"configuraciones\".
+La lista puede ser creada con el módulo de \"configuracion\".
 """
 
 import socket
@@ -18,7 +18,7 @@ def obtener_ntp(direccion):
         direccion (str): Direccion de un servidor.
 
     Returns:
-        int: Devuelve un entero con los datos de la hora.
+        tiempo (int): Devuelve un entero con los datos de la hora.
 
     Errors:
         None: Tiempo de espera de la petición al servidor expirado.
@@ -28,7 +28,7 @@ def obtener_ntp(direccion):
     mensaje = "\x1b" + 47 * "\0"
     try:
         cliente.settimeout(5.0)
-        cliente.sendto( mensaje.encode(), (direccion, 123))
+        cliente.sendto(mensaje.encode(), (direccion, 123))
         datos, _ = cliente.recvfrom( 1024 )
         if datos:
             tiempo = struct.unpack( "!12I", datos )[10]
